0668d4b1229e7ee18dca8e54f1ae4a4ebe2ea7e8
Raymond <yuraymond96@gmail.com>
2019-09-22 13:15:18 -0700

    Update main.c
    Added comments

--- Lab2/main.c
+++ Lab2/main.c
@@ -15,11 +15,13 @@ int main(void)
 //	CSCTL1 |= DCOFSEL1 + DCOFSEL0; //alternate method? to set 8MHz; (pg81 ug) for high high
 	CSCTL2 |= SELS__DCOCLK; // sets SMCLK to run off DCO (pg82 ug), 011b = DCOCLK
 //	CSCTL2 |= SELS1 + SELS0; // alternate method? to set SMCLK to run off DCO; (pg82 ug) for low high high
-	CSCTL3 |= DIVS2 + DIVS0; // sets SMCLK divider to /32 or 2^5 (pg83 ug) (or pg83 ug); 101 = high low high, DIVS1 is low
+	CSCTL3 |= DIVS2 + DIVS0; // sets SMCLK divider to /32 (pg83 ug); 101 = high low high, DIVS1 is low
 	P3DIR = BIT3; // change P3.4 direction
 	P3OUT |= BIT3; // set P3.4 as output
 	P3SEL1 |= BIT3; // enable P3.4 to output SMCLK (pg81 ds)
 	P3SEL0 |= BIT3; // enable P3.4 to outputSMCLK (pg81 ds)
+
+	/*Exercise 2*/
 
 	return 0;
 }
