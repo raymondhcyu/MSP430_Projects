d4c499cdcb030ff02c56b4a8e0c0f354a2964aee
Raymond <yuraymond96@gmail.com>
2019-09-22 13:14:06 -0700

    Added alternate methods
    For bitsetting

--- Lab2/main.c
+++ Lab2/main.c
@@ -10,11 +10,16 @@ int main(void)
 
 	/*Exercise 1*/
 	CSCTL0_H = CSKEY >> 8; // enables CS registers, can also do = 0xA5 (pg80 ug [ug = user guide])
-	CSCTL1 |= DCOFSEL3; // enables 8MHz DCO (pg16 ds [ds = datasheet])
-	CSCTL2 |= SELS__DCOCLK; // sets SMCLK to run off DCO (pg82 ug), 011b = DCOCLK (unsure how to set this another way)
-	CSCTL3 &= ~DIVS__5; // sets divider to /32 or 2^5 (pg83 ug)
-	P3DIR |= BIT3; // change P3.4 direction
+	CSCTL1 &= ~DCORSEL; // DCORSEL set to 0
+	CSCTL1 |= DCOFSEL_3; // enables 8MHz DCO (pg16 ds [ds = datasheet]
+//	CSCTL1 |= DCOFSEL1 + DCOFSEL0; //alternate method? to set 8MHz; (pg81 ug) for high high
+	CSCTL2 |= SELS__DCOCLK; // sets SMCLK to run off DCO (pg82 ug), 011b = DCOCLK
+//	CSCTL2 |= SELS1 + SELS0; // alternate method? to set SMCLK to run off DCO; (pg82 ug) for low high high
+	CSCTL3 |= DIVS2 + DIVS0; // sets SMCLK divider to /32 or 2^5 (pg83 ug) (or pg83 ug); 101 = high low high, DIVS1 is low
+	P3DIR = BIT3; // change P3.4 direction
 	P3OUT |= BIT3; // set P3.4 as output
+	P3SEL1 |= BIT3; // enable P3.4 to output SMCLK (pg81 ds)
+	P3SEL0 |= BIT3; // enable P3.4 to outputSMCLK (pg81 ds)
 
 	return 0;
 }
