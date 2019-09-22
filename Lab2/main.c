cd1636163d12a18b79cb7a05dc4879241307204d
Raymond <yuraymond96@gmail.com>
2019-09-22 14:30:35 -0700

    Alternate method to using __
    Hey it works

--- Lab2/main.c
+++ Lab2/main.c
@@ -11,10 +11,10 @@ int main(void)
 	/*Exercise 1*/
 	CSCTL0_H = CSKEY >> 8; // enables CS registers, can also do = 0xA5 (pg80 ug [ug = user guide])
 	CSCTL1 &= ~DCORSEL; // DCORSEL set to 0
-//	CSCTL1 |= DCOFSEL_3; // enables 8MHz DCO (pg16 ds [ds = datasheet]
-	CSCTL1 |= DCOFSEL0 + DCOFSEL1; //alternate method? to set 8MHz; (pg81 ug) for high high
-//	CSCTL2 |= SELS__DCOCLK; // sets SMCLK to run off DCO (pg82 ug), 011b = DCOCLK
-	CSCTL2 |= SELS0 + SELS1; // alternate method? to set SMCLK to run off DCO; (pg82 ug) for low high high
+	CSCTL1 |= DCOFSEL_3; // enables 8MHz DCO (pg16 ds [ds = datasheet]
+//	CSCTL1 |= DCOFSEL0 + DCOFSEL1; // alternate method to set 8MHz; (pg81 ug) for high high
+	CSCTL2 |= SELS__DCOCLK; // sets SMCLK to run off DCO (pg82 ug), 011b = DCOCLK
+//	CSCTL2 |= SELS0 + SELS1; // alternate method to set SMCLK to run off DCO; (pg82 ug) for low high high
 	CSCTL3 |= DIVS0 + DIVS2; // sets SMCLK divider to /32 (pg83 ug); 101 = high low high, DIVS1 is low
 	P3DIR = BIT4; // change P3.4 direction
 	P3OUT |= BIT4; // set P3.4 as output
