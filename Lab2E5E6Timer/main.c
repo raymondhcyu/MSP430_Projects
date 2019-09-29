eb5e0c0d8baf678e251bc2b3e5fea153ad5d5ae5
Raymond <yuraymond96@gmail.com>
2019-09-29 16:37:50 -0700

    Nonfunctioning commit
    Target not loaded, suspect problem with interrupt

--- Lab2E5E6Timer/main.c
+++ Lab2E5E6Timer/main.c
@@ -1,5 +1,11 @@
 #include <msp430.h>
 
+// Define global vars for interrupt
+volatile unsigned int rise;
+volatile unsigned int fall;
+volatile unsigned int freq = 0; // falling - rising
+volatile unsigned int mark = 0; // record if rising or falling
+volatile int currentFreq;
 
 /**
  * main.c
@@ -18,10 +24,7 @@ int main(void)
     // Set timer B
     TB1CTL |= TBSSEL1 + MC0; // select SMCLK source, initialize up mode (ug372)
 
-    // Set timer A
-    TA1CTL |= TASSEL1 + MC0; // select SMCLK source, initialize up mode (ug349)
-
-    // Set output modes (ug366 diagrams)
+    // Set mode (ug375, ug366 diagrams)
     TB1CCTL1 = OUTMOD_3; // set capture/compare register to set/reset (ug375)
     TB1CCTL2 = OUTMOD_3; // set capture/compare register to set/reset (ug375)
 
@@ -30,12 +33,47 @@ int main(void)
     TB1CCR1 = 1000; // 50% duty cycle, TB1.1 50%
     TB1CCR2 = 1500; // 25% duty cycle, TB1.2 25%
 
-    // Set P3 to be Timer B output and LED output (P1 only has Timer A and no LED); um14, 17
+    // Set P3.4 and P3.5 to be Timer B output and LED output (P1 only has Timer A and no LED); um14, 17
     // BIT4 is TB1.1, BIT5 is TB1.2, should be dimmer
     P3DIR |= BIT4 + BIT5;
     P3OUT &= ~(BIT4 + BIT5);
     P3SEL0 |= BIT4 + BIT5;
     P3SEL1 &= ~(BIT4 + BIT5);
 
+    // Set timer A
+    TA1CTL |= TASSEL1 + MC0; // select SMCLK source, initialize up mode (ug349)
+
+    // Set mode (ug351, ug366 diagrams)
+    TA1CCTL1 = CM0 + CM1 + SCS + CAP + CCIE; // capture on rise + falling edge, synchronize capture with timer clock, set to capture, enable interrupt
+
+    // Set P1.2 to receiver Timer B input (ds7 P1.2 has TA1.1)
+    P1DIR &= ~BIT2; // set P1.2 to input ug293
+    P1SEL0 &= ~BIT2;
+    P1SEL1 = BIT2;
+
+    // Set P3.7 to be freq output
+    P3DIR |= BIT7;
+    P3OUT &= BIT7;
+
+    _EINT(); // enable global interrupts
+
 	return 0;
+}
+
+#pragma vector = TIMER0_A1_VECTOR
+__interrupt void Timer_A(void)
+{
+    currentFreq = TA1CCR0;
+    if (mark == 0)
+    {
+        rise = currentFreq;
+        mark = 1;
+        P3OUT ^= BIT7;
+    }
+    else
+    {
+        fall = currentFreq;
+        mark = 0;
+    }
+    freq = fall - rise;
 }
