f6f761502a84753af70186d8304f04d41e1f7036
Raymond <yuraymond96@gmail.com>
2019-11-03 17:34:36 -0800

    Added Timers A and B

--- Lab3Ex4/main.c
+++ Lab3Ex4/main.c
@@ -59,5 +59,50 @@ int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
+	// Receive inputs from P1.1 and P1.2
+	P1DIR &= ~(BIT1 BIT2);
+	P1SEL1 |= BIT1 + BIT2;
+	P1SEL0 &= ~(BIT1 + BIT2);
+
+	setTimer();
+	setUART();
+
 	return 0;
+}
+
+#pragma vector = USCI_A1_VECTOR
+__interrupt void USCI_A1_ISR(void)
+{
+    unsigned char RxByte = 0;
+    RxByte = UCA1RXBUF; // get val from RX buffer
+    enqueue(queue, RxByte);
+}
+
+void setClk() {
+    CSCTL0_H = CSKEY >> 8; // enables CS registers, can also do = 0xA5 (pg80 ug [ug = user guide])
+    CSCTL1 &= ~DCORSEL; // DCORSEL set to 0 ug72
+    CSCTL1 |= DCOFSEL0 + DCOFSEL1; // (pg81 ug) for 8MHz 11b
+    CSCTL2 |= SELM0 + SELM1 + SELA0 + SELA1 + SELS0 + SELS1; // set all CLK to run off DCO; (ug82)
+    CSCTL3 |= DIVS__8; // set SMCLK divider to /8
+}
+
+void setTimer() {
+    // Set up timer A to receive input as clock
+    TA0CTL |= TASSEL_0 + MC_2;
+    TA1CTL |= TASSEL_0 + MC_2;
+
+    // Set up timer B to transmit data through UART
+    TB0CTL |= TBSSEL_2 + MC_2;
+    TB0CCTL0 |= OUTMOD_3 + CCIE;
+
+}
+
+void setUART() {
+    P2SEL0 &= ~(RXD + TXD); // set to 00 ds74
+    P2SEL1 |= RXD + TXD; // set to 11 ds74
+    UCA1CTLW0 = UCSSEL0; // 01b for ACLK (pg495 ug)
+    UCA1MCTLW = UCOS16 + UCBRF0 + 0x4900; // 9600 baud from 8MHz ug490; UCOS16 = oversampling enabled, UCBRF0 = modulation stage
+//    UCA1MCTLW = UCOS16 + UCBRF3 + UCBRF1 + 0xF700; // 57600 baud; UCBRFx = decimal 10 = 1010 hex = high low high low
+    UCA1BRW = 52; // ug490 and ug497, bit clock prescaler ***Why is this 52 for both 9600 and 57600 baud?
+    UCA1IE |= UCRXIE; // enable UART RX interrupt
 }
